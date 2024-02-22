bb02cb8c2c0a74ca6bff337fa19c14444ac8df80
grace9350 <86778935+grace9350@users.noreply.github.com>
2024-02-22 18:23:46 +0900

    Update N_2775_부녀회장이될테야.py

--- N_2775_부녀회장이될테야.py
+++ N_2775_부녀회장이될테야.py
@@ -1,28 +1,3 @@
-"""def apartment(k, n):
-    if k>0 & listN[k][n] == 0:
-        for i in range(n + 1):
-            apartment(k-1, i)
-        sum = 0
-        for i in range(n + 1):
-            sum += listN[k-1][i]
-        listN[k][n] = sum
-       
-listN = [[i+1 for i in range(14)]]
-row = 0
-
-T = int(input())
-for i in range(T):
-    k = int(input())
-    n = int(input()) - 1
-    if row < k:
-        for j in range(k - row):
-            listN.append([0 for k in range(14)])
-        row = k
-    apartment(k, n)
-    print(listN[k][n])
-"""
-#시간초과
-
 T = int(input())
 for i in range(T):
     k = int(input())
